3bc471f461e968f378c0089a29216e77b2633938
kyrsten-note <kyrstenjr@gmail.com>
2023-11-19 14:42:52 -0300

    criação da tabela

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
-export class CoursesComponent {
 
+export class CoursesComponent {
+  courses: Course[] = [];
+  displayedColumns = ['name', 'category'];
 }
