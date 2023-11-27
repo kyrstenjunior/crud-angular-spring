b415ed220646947776a137603949633b18b02099
kyrsten-note <kyrstenjr@gmail.com>
2023-11-26 21:13:48 -0300

    recriação do projeto

--- crud-spring/src/main/java/com/junior/crudspring/controller/HelloController.java
+++ crud-spring/src/main/java/com/junior/crudspring/controller/HelloController.java
@@ -1,16 +1,14 @@
-package com.junior.controller;
+package com.junior.crudspring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("teste")
+@RequestMapping("/")
 public class HelloController {
-
-  @GetMapping()
-  public String hello() {
-    return "Hello!";
-  }
-
+    @GetMapping
+    public String hello() {
+        return "Hello";
+    }
 }
